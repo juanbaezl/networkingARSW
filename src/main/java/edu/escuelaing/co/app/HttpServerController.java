8ad2fc3e4413136b254d720934e7f21a05c7bbd6
juanbaezl <juan.baez-l@mail.escuelaing.edu.co>
2022-06-16 12:22:41 -0500

    heroku puerto

--- src/main/java/edu/escuelaing/co/app/HttpServerController.java
+++ src/main/java/edu/escuelaing/co/app/HttpServerController.java
@@ -1,8 +1,5 @@
 package edu.escuelaing.co.app;
 
-import java.io.PrintWriter;
-import java.net.Socket;
-
 /**
  * Clase que sirve de controlador del servidor web
  */
@@ -16,18 +13,7 @@ public class HttpServerController {
     public static void main(String[] args) {
         HttpServer servidor = HttpServer.getInstance();
         try {
-            if (args[0].equals("start")) {
-                servidor.start();
-            } else if (args[0].equals("stop")) {
-                Socket echoSocket = new Socket("127.0.0.1", 35000);
-                PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
-                out.println("exit");
-                System.out.println("Se ha apagado el servidor web");
-                out.close();
-                echoSocket.close();
-            } else {
-                System.out.println("Solo se acepta como argumento stop o start");
-            }
+            servidor.start();
         } catch (Exception e) {
             e.printStackTrace();
         }
